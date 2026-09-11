1b073f03c1101289152b94ea77e1f30075af8328
Varad Rajiv Pandare <varadpandare.24@kgpian.iitkgp.ac.in>
2026-09-12 03:52:07 +0530

    style: revert text to cute selfie

--- app/src/main/java/com/varad/unstoppableash/ui/TravelerChatScreen.kt
+++ app/src/main/java/com/varad/unstoppableash/ui/TravelerChatScreen.kt
@@ -159,7 +159,7 @@ fun TravelerChatScreen(onBack: () -> Unit) {
                     }
                     Spacer(modifier = Modifier.width(12.dp))
                     Text(
-                        text = "Tap to send a cute snap!",
+                        text = "Tap to send a cute selfie!",
                         fontWeight = FontWeight.SemiBold,
                         color = PremiumAccent,
                         modifier = Modifier
