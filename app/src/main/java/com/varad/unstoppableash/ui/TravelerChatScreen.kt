d5dcb8c6a65efdc106c86fd68368daef168d84b1
Varad Rajiv Pandare <varadpandare.24@kgpian.iitkgp.ac.in>
2026-09-12 03:57:29 +0530

    style: shorten selfie prompt text

--- app/src/main/java/com/varad/unstoppableash/ui/TravelerChatScreen.kt
+++ app/src/main/java/com/varad/unstoppableash/ui/TravelerChatScreen.kt
@@ -159,7 +159,7 @@ fun TravelerChatScreen(onBack: () -> Unit) {
                     }
                     Spacer(modifier = Modifier.width(12.dp))
                     Text(
-                        text = "tap here to make varad happy by sending a cute selfie",
+                        text = "Make Varad happy with a cute selfie!",
                         fontWeight = FontWeight.SemiBold,
                         color = PremiumAccent,
                         modifier = Modifier
